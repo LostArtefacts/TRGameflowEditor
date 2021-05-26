3fb2ef579e3ce632384673026a111d9c27b2736b
lamb86 <lam8667@gmail.com>
2021-05-26 16:11:30 +0100

    #85 Config checks
    More stringent checks performed on ammoless/unarmed level counts.

--- TRGE.Core/Script/Impls/TR23ScriptEditor.cs
+++ TRGE.Core/Script/Impls/TR23ScriptEditor.cs
@@ -30,7 +30,7 @@ namespace TRGE.Core
             Config unarmedLevels = config.GetSubConfig("UnarmedLevels");
             UnarmedLevelOrganisation = unarmedLevels.GetOrganisation("Organisation");
             UnarmedLevelRNG = new RandomGenerator(unarmedLevels.GetSubConfig("RNG"));
-            RandomUnarmedLevelCount = unarmedLevels.GetUInt("RandomCount");
+            RandomUnarmedLevelCount = Math.Min(unarmedLevels.GetUInt("RandomCount"), (LevelManager as TR23LevelManager).GetUnarmedLevelCount());
             //see note in base.LoadConfig re restoring randomised - same applies for Unarmed
             if (unarmedLevels.ContainsKey("Data"))
             {
@@ -40,7 +40,7 @@ namespace TRGE.Core
             Config ammolessLevels = config.GetSubConfig("AmmolessLevels");
             AmmolessLevelOrganisation = ammolessLevels.GetOrganisation("Organisation");
             AmmolessLevelRNG = new RandomGenerator(ammolessLevels.GetSubConfig("RNG"));
-            RandomAmmolessLevelCount = ammolessLevels.GetUInt("RandomCount");
+            RandomAmmolessLevelCount = Math.Min(ammolessLevels.GetUInt("RandomCount"), (LevelManager as TR23LevelManager).GetAmmolessLevelCount());
             //see note in base.LoadConfig re restoring randomised - same applies for Ammoless
             if (ammolessLevels.ContainsKey("Data"))
             {
