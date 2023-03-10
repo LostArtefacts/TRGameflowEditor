e94909ed8591325f4a14bc866d9ce01fc3add126
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-03-10 11:17:05 +0000

    #114 SDK Format
    Resolves #114.

--- TRGE.View/App.xaml.cs
+++ TRGE.View/App.xaml.cs
@@ -27,21 +27,7 @@ namespace TRGE.View
             Assembly assembly = Assembly.GetExecutingAssembly();
             Version v = assembly.GetName().Version;
             Version = string.Format("{0}.{1}.{2}", v.Major, v.Minor, v.Build);
-
-            object[] attributes = assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-            if (attributes.Length > 0)
-            {
-                TaggedVersion = ((AssemblyProductAttribute)attributes[0]).Product.Trim();
-                if (TaggedVersion.Contains(" "))
-                {
-                    string[] tagArr = TaggedVersion.Split(' ');
-                    TaggedVersion = tagArr[tagArr.Length - 1];
-                }
-            }
-            else
-            {
-                TaggedVersion = "v" + Version;
-            }
+            TaggedVersion = "V" + Version;
 
             TRInterop.ExecutingVersion = Version;
             TRInterop.TaggedVersion = TaggedVersion;
